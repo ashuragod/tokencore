48f141ad19857cb97d7868314bee2a6a31634e12
ashuragod <253729909@qq.com>
2022-04-12 07:33:32 +0800

    new
    new

---
+++ src/main/java/org/consenlabs/tokencore/wallet/address/FilecoinAddressCreator.java
@@ -0,0 +1,41 @@
+package org.consenlabs.tokencore.wallet.address;
+
+import cn.hutool.core.codec.Base32;
+import cn.hutool.core.util.HexUtil;
+import com.filecoinj.crypto.ECKey;
+import org.consenlabs.tokencore.foundation.utils.NumericUtil;
+import ove.crypto.digest.Blake2b;
+
+import java.util.Arrays;
+
+public class FilecoinAddressCreator  implements AddressCreator{
+
+    private String publicKeyToAddress(byte[] pubKeyBytes) {
+        Blake2b.Digest digest = Blake2b.Digest.newInstance(20);
+        String hash = HexUtil.encodeHexStr(digest.digest(pubKeyBytes));
+
+        String pubKeyHash = "01" + HexUtil.encodeHexStr(digest.digest(pubKeyBytes));
+
+        Blake2b.Digest blake2b3 = Blake2b.Digest.newInstance(4);
+        String checksum = HexUtil.encodeHexStr(blake2b3.digest(HexUtil.decodeHex(pubKeyHash)));
+
+        return "f1" + Base32.encode(HexUtil.decodeHex(hash + checksum)).toLowerCase();
+    }
+
+    private String fromECKey(ECKey key) {
+        byte[] pubKeyBytes = key.getPubKey();
+        return publicKeyToAddress(pubKeyBytes);
+    }
+
+    @Override
+    public String fromPrivateKey(String prvKeyHex) {
+        ECKey key = ECKey.fromPrivate(NumericUtil.hexToBytes(prvKeyHex));
+        return fromECKey(key);
+    }
+
+    @Override
+    public String fromPrivateKey(byte[] prvKeyBytes) {
+        ECKey key = ECKey.fromPrivate(prvKeyBytes);
+        return fromECKey(key);
+    }
+}
--- src/main/java/org/consenlabs/tokencore/wallet/transaction/TronTransaction.java
+++ src/main/java/org/consenlabs/tokencore/wallet/transaction/TronTransaction.java
@@ -1,99 +1,133 @@
 package org.consenlabs.tokencore.wallet.transaction;
 
-import com.google.protobuf.Any;
-import com.google.protobuf.ByteString;
+
 import org.consenlabs.tokencore.wallet.Wallet;
-import org.tron.common.crypto.ECKey;
-import org.tron.common.crypto.Sha256Sm3Hash;
-import org.tron.common.utils.ByteArray;
-import org.tron.common.utils.TransactionUtils;
-import org.tron.protos.Protocol;
-import org.tron.protos.contract.BalanceContract;
-import org.tron.walletserver.WalletApi;
+import org.consenlabs.tokencore.wallet.model.Messages;
+import org.consenlabs.tokencore.wallet.model.TokenException;
+import org.spongycastle.util.encoders.Hex;
+import org.tron.trident.abi.FunctionEncoder;
+import org.tron.trident.abi.TypeReference;
+import org.tron.trident.abi.datatypes.Address;
+import org.tron.trident.abi.datatypes.Bool;
+import org.tron.trident.abi.datatypes.Function;
+import org.tron.trident.abi.datatypes.generated.Uint256;
+import org.tron.trident.core.ApiWrapper;
+import org.tron.trident.core.exceptions.IllegalException;
+import org.tron.trident.crypto.SECP256K1;
+import org.tron.trident.crypto.tuwenitypes.Bytes32;
+import org.tron.trident.proto.Chain;
+import org.tron.trident.proto.Contract;
+import org.tron.trident.proto.Response;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 public class TronTransaction implements TransactionSigner {
 
-    private final String from;
+    private final static long DUST_THRESHOLD = 0;
 
-    private final String to;
-
-    private final Long amount;
-
-
-
-    public TronTransaction(String from, String to, Long amount) {
+    public TronTransaction(String from, String to, long amount) {
         this.from = from;
         this.to = to;
         this.amount = amount;
-    }
-
-    public static Protocol.Transaction setReference(Protocol.Transaction transaction,Protocol.Block newestBlock) {
-        long blockHeight = newestBlock.getBlockHeader().getRawData().getNumber();
-
-        byte[] blockHash = getBlockHash(newestBlock).getBytes();
-        byte[] refBlockNum = ByteArray.fromLong(blockHeight);
-        Protocol.Transaction.raw rawData = transaction.getRawData().toBuilder()
-                .setRefBlockHash(ByteString.copyFrom(ByteArray.subArray(blockHash, 8, 16)))
-                .setRefBlockBytes(ByteString.copyFrom(ByteArray.subArray(refBlockNum, 6, 8)))
-                .build();
-        return transaction.toBuilder().setRawData(rawData).build();
-    }
-
-    public static Sha256Sm3Hash getBlockHash(Protocol.Block block) {
-        return Sha256Sm3Hash.of(block.getBlockHeader().getRawData().toByteArray());
-    }
-
-    public  String getTransactionHash(Protocol.Transaction transaction) {
-        return ByteArray.toHexString(Sha256Sm3Hash.hash(transaction.getRawData().toByteArray()));
-    }
-
-    public static Protocol.Transaction createTransaction(byte[] from, byte[] to, long amount) {
-        Protocol.Block newestBlock=WalletApi.getBlock(-1);
-        Protocol.Transaction.Builder transactionBuilder = Protocol.Transaction.newBuilder();
-        Protocol.Transaction.Contract.Builder contractBuilder = Protocol.Transaction.Contract.newBuilder();
-        BalanceContract.TransferContract.Builder transferContractBuilder = BalanceContract.TransferContract.newBuilder();
-        transferContractBuilder.setAmount(amount);
-        ByteString bsTo = ByteString.copyFrom(to);
-        ByteString bsOwner = ByteString.copyFrom(from);
-        transferContractBuilder.setToAddress(bsTo);
-        transferContractBuilder.setOwnerAddress(bsOwner);
-        try {
-            Any any = Any.pack(transferContractBuilder.build());
-            contractBuilder.setParameter(any);
-        } catch (Exception e) {
-            return null;
+        if (amount < DUST_THRESHOLD) {
+            throw new TokenException(Messages.AMOUNT_LESS_THAN_MINIMUM);
         }
-        contractBuilder.setType(Protocol.Transaction.Contract.ContractType.TransferContract);
-        transactionBuilder.getRawDataBuilder().addContract(contractBuilder)
-                .setTimestamp(System.currentTimeMillis())
-                .setExpiration(newestBlock.getBlockHeader().getRawData().getTimestamp() + 10 * 60 * 60 * 1000);
-        Protocol.Transaction transaction = transactionBuilder.build();
-        return setReference(transaction, newestBlock);
     }
 
+    public TronTransaction(String from, String to, int tokenId, long amount) {
+        this.from = from;
+        this.to = to;
+        this.tokenId = tokenId;
+        this.amount = amount;
+        if (amount < DUST_THRESHOLD) {
+            throw new TokenException(Messages.AMOUNT_LESS_THAN_MINIMUM);
+        }
+    }
+
+    public TronTransaction(String from, String to, String contractAddress, long amount) {
+        this.from = from;
+        this.to = to;
+        this.contractAddress = contractAddress;
+        this.amount = amount;
+        if (amount < DUST_THRESHOLD) {
+            throw new TokenException(Messages.AMOUNT_LESS_THAN_MINIMUM);
+        }
+    }
+
+    private String from;
+
+    private String to;
+
+    private long amount;
+
+    private int tokenId;
+
+    private String contractAddress;
+
+    ApiWrapper client = ApiWrapper.ofMainnet("");
 
     @Override
     public TxSignResult signTransaction(String chainId, String password, Wallet wallet) {
-        String privateStr = wallet.exportPrivateKey(password);
-        byte[] privateBytes = ByteArray.fromHexString(privateStr);
-        ECKey ecKey = ECKey.fromPrivate(privateBytes);
-        Protocol.Transaction transaction = createTransaction(WalletApi.decodeFromBase58Check(from), WalletApi.decodeFromBase58Check(to), amount);
-        assert transaction != null;
-        transaction = TransactionUtils.sign(transaction, ecKey);
-        String txHash=getTransactionHash(transaction);
-        return new TxSignResult(ByteArray.toHexString(transaction.toByteArray()), txHash);
+
+        String hexPrivateKey = wallet.exportPrivateKey(password);
+        SECP256K1.KeyPair keyPair = SECP256K1.KeyPair.create(SECP256K1.PrivateKey.create(Bytes32.fromHexString(hexPrivateKey)));
+        Response.TransactionExtention txnExt;
+        try {
+
+            txnExt = client.transfer(from, to, amount);
+            Chain.Transaction signedTransaction = client.signTransaction(txnExt, keyPair);
+            String txid = Hex.toHexString(txnExt.getTxid().toByteArray());
+            return new TxSignResult(signedTransaction.toString(), txid);
+        } catch (IllegalException e) {
+            throw new TokenException("签名失败 原因", e);
+        }
     }
 
-    public static void main(String[] args) {
-
-        String from="TGA1KxMbBvYWL2C4iirBPMz3Lt1BFoHB1D";
-        String to="TGA1KxMbBvYWL2C4iirBPMz3Lt1BFoHB1D";
-        long amount=0L;
-        Protocol.Transaction transaction = createTransaction(WalletApi.decodeFromBase58Check(from), WalletApi.decodeFromBase58Check(to), amount);
-        assert transaction != null;
-        System.out.println(ByteArray.toHexString(transaction.toByteArray()));
-
-
+    public TxSignResult signTrc10Transaction(String chainId, String password, Wallet wallet) {
+        String hexPrivateKey = wallet.exportPrivateKey(password);
+        SECP256K1.KeyPair keyPair = SECP256K1.KeyPair.create(SECP256K1.PrivateKey.create(Bytes32.fromHexString(hexPrivateKey)));
+        Response.TransactionExtention txnExt;
+        try {
+            txnExt = client.transferTrc10(from, to, tokenId, amount);
+            Chain.Transaction signedTransaction = client.signTransaction(txnExt, keyPair);
+            String txid = Hex.toHexString(txnExt.getTxid().toByteArray());
+            return new TxSignResult(signedTransaction.toString(), txid);
+        } catch (IllegalException e) {
+            throw new TokenException("签名失败 原因", e);
+        }
     }
+
+
+    public TxSignResult signTrc20Transaction(String chainId, String password, Wallet wallet) {
+        String hexPrivateKey = wallet.exportPrivateKey(password);
+        SECP256K1.KeyPair keyPair = SECP256K1.KeyPair.create(SECP256K1.PrivateKey.create(Bytes32.fromHexString(hexPrivateKey)));
+        // transfer(address,uint256) returns (bool)
+        Function trc20Transfer = new Function("transfer",
+                Arrays.asList(new Address(to),
+                        new Uint256(BigInteger.valueOf(amount))),
+                Collections.singletonList(new TypeReference<Bool>() {
+                }));
+
+        String encodedHex = FunctionEncoder.encode(trc20Transfer);
+
+        Contract.TriggerSmartContract trigger =
+                Contract.TriggerSmartContract.newBuilder()
+                        .setOwnerAddress(ApiWrapper.parseAddress(from))
+                        .setContractAddress(ApiWrapper.parseAddress(contractAddress))
+                        .setData(ApiWrapper.parseHex(encodedHex))
+                        .build();
+
+        Response.TransactionExtention txnExt = client.blockingStub.triggerContract(trigger);
+        String txid = Hex.toHexString(txnExt.getTxid().toByteArray());
+
+        Chain.Transaction signedTxn = client.signTransaction(txnExt, keyPair);
+
+        return new TxSignResult(signedTxn.toString(), txid);
+    }
+
 }
+
 
